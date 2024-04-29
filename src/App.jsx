acf81c0f3e8df545d0f9a8db5cdb5ddc7ffef485
Markus Machel <machelmarkus@gmail.com>
2024-04-29 01:20:00 -0300

    projeto base

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
-import HomePage from './pages/Home';
-
-const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  {}
-]);
 
 function App() {
 
   return (
-    <RouterProvider router={router} />
+    <p>Inside the app component!</p>
   )
 }
 
